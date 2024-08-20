cff225de8f8993150e9142ca2445c1994504947f
mTy8421 <koontyza007@gmail.com>
2024-08-20 17:51:49 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
+  req.cookies['test'] = 'test3';
   const data = [
     { hello: 'hello', test: 'test' },
-    { hello: 'hello2', test: 'test2' }
-  ]
-  res.json(data)
+    { hello: 'hello2', test: 'test2' },
+    { hello: 'hello2', test: req.cookies['test'] },
+  ];
+  res.json(data);
+  res.end();
 });
 
 module.exports = router;
